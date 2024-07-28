24c927f6b2988890acc86874f769edded39b5fa1
BelikAE <BelikAE@HOME-PC>
2024-07-28 18:36:31 +1000

    Выдача

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lab1
@@ -10,6 +11,26 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число");
+            double n = Convert.ToDouble(Console.ReadLine());
+            FactorialAsync(n);
+            Console.ReadKey();
+        }
+
+        static void Factorial(double n)
+        {
+            double s = 1;
+            for (int i = 1; i < n+1; i++)
+            {
+                s *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine(s); 
+        }
+
+        static async void FactorialAsync(double n)
+        {
+            await Task.Run(() => Factorial(n));
         }
     }
 }
